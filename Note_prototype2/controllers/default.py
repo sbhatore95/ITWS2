157fbeb1266926e5cf302425fe1b16de727de0cd
Kushal <kushal@kushal-Inspiron-3521.(none)>
2014-04-05 23:20:24 +0530

    Second Prototype 5th April

---
+++ Note_prototype2/controllers/default.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Welcome to web2py!")
+    return dict(message=T('Hello World'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+def view_all():
+    var = db().select(db.Notes.ALL)
+    return dict(notes=var)
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
+
+def table_update():
+    title = request.get_vars['Title'];
+    info =request.get_vars['Info'];
+    modified =request.get_vars['Modified'];
+
+    db.Notes.insert(Title=title,Info=info,Modified=modified)
+
+
+    return dict(j="True")
+
+def auto_suggestion():
+    suggestion=request.get_vars['sug']
+    rows=db(db.Notes.Info.contains(suggestion)).select()
+    ans='{"names":['
+    count=0
+    for i in rows:
+        if(count!=0):
+          ans+= ","
+        ans+= '"'+ i["Title"] +'"'
+        count+=1
+    ans+= "]}"
+
+      
+    return dict(ans=ans)
+
+#db(db.mytable.myfield.contains('text')).select() 
+# relational database 
+#db(db.mytable.myfield.contains('text')).select() 
+
+def main():
+   return dict()
+
+
+def open():
+    temp = request.args(0);
+    var=db().select(db.Notes.ALL)
+    title = ""
+    ans = temp.strip()
+    for i in ans:
+        if i is '_':
+            title+=' '
+        else:
+            title+=i
+
+    for j in var:
+        if j.Title == title:
+            content = j.Info
+
+    return dict(ans=title, ans2=content)
